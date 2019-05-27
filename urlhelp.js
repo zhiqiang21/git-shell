ce89067c55e28fcb633806780e379a4f71c76392
zhiqiang21 <hpuhouzhiqiang@gmail.com>
2019-05-27 14:11:30 +0800

    refactor:重构urlhelp去掉对query-string依赖，增加对不标准url的解析

--- urlhelp.js
+++ urlhelp.js
@@ -6,53 +6,116 @@
  * @author houzhiqiang@gmail.com
  */
 
+//  https://www.baidu.com?aa=111#/hash
+// https://www.baidu.com/#/aaaa?bbb=1
+//  https://www.baidu.com?aa=111#/hash?bbb=2&aa=222
 
-const queryString = require('query-string');
+// 因为平时大家url中拼接参数时的不规范导致 hash 和 query参数的获取错误以上列举了四种可能的情况
 
 const args = process.argv;
 
+if (args.length < 3) {
+    return 'please input you url string !';
+}
 
-outPutFn(args.slice(2));
+const urlString = args.slice(2);
+// const urlString = 'https://www.baidu.com?aa=111#/hash?bbb=2&aa=222';
+// const urlString = 'https://www.baidu.com?aa=111#/hash';
+// const urlString = 'https://www.baidu.com/#/aaaa?bbb=1';
 
-/**
- * 输入命令的方式
- * urlhelp hash  [url]   只输出hash对象
- * urlhelp query [url]   只输出query对象
- * urlhelp qh [url]   /  urlhelp [url]    输出query和hash对象
- * @param {*} params
- */
-function outPutFn(params) {
 
-  if (params.length === 1) {
-    getQueryObj(params[0].substring(params[0].indexOf('?')));
-    // getHashObj(urlSearch);
-  } else {
-    const urlSearch = params[1].substring(params[1].indexOf('?'));
+const hashIndex = urlString.indexOf('#');
+const searchIndex = urlString.indexOf('?');
 
-    if (params[0] === 'hash') {
-      getHashObj(urlSearch);
+const hash = getHashParams(hashIndex, urlString);
+const searchObj = getSearchParams(searchIndex, urlString);
+
+if (hash) {
+    console.log('===============hash 参数==============');
+    console.log(`hash：${hash}`);
+}
+
+if (JSON.stringify(searchObj) !== '{}') {
+    console.log('=============query 参数列表================');
+
+    for (let item in searchObj) {
+        if (searchObj.hasOwnProperty(item)) {
+            console.log(`${item}：${searchObj[item]}`);
+        }
     }
-
-    if (params[0] === 'query' || params[0] === 'q') {
-      getQueryObj(urlSearch);
-    }
-  }
 }
 
 
-function getQueryObj(url) {
-  console.log('*************query对象****************');
-  const queryObj = queryString.parse(url);
+// 获取query参数
+function getSearchParams(index, url) {
 
-  for (let item in queryObj) {
-    console.log(`${item} --> ${queryObj[item]}`);
-  }
-  console.log('*******************************');
+    const searchObj = {};
+
+    if (index < 0) {
+        return null;
+    }
+
+    const search = url.substring(index + 1);
+    const hashIndex = search.indexOf('#');
+
+    //  https://www.baidu.com?aa=111#/hash?bbb=2&aa=222
+    if (hashIndex) {
+        const before = search.substring(0, hashIndex);
+        const hashString = search.substring(hashIndex);
+
+        if (hashString.indexOf('?') >= 0) {
+            const queryString = before + '&' + hashString.substring(hashString.indexOf('?') + 1);
+
+            return stringToObj(queryString);
+        } else {
+
+            return stringToObj(before);
+        }
+
+
+    } else {
+        const searchArr = search.split('&');
+
+        searchArr.forEach(item => {
+            const arr = item.split('=');
+
+            searchObj[arr[0]] = arr[1];
+        });
+    }
+
+    return searchObj;
+
 }
 
 
-function getHashObj(url) {
-  console.log('*************hash对象****************');
-  console.log(queryString.parse(url));
-  console.log('*******************************');
+// 获取hash参数
+function getHashParams(index, url) {
+    if (index < 0) {
+        return null;
+    }
+
+    const hashString = url.substring(url.indexOf('#') + 2);
+
+    if (hashString.indexOf('?') >= 0) {
+        return hashString.substring(0, hashString.indexOf('?'));
+
+    } else {
+        return hashString;
+    }
+
+
+}
+
+// 转化str为 obj对象
+function stringToObj(str) {
+    const strArr = str.split('&');
+    const obj = {};
+
+    strArr.forEach(item => {
+        const arr = item.split('=');
+
+        obj[arr[0]] = arr[1];
+    });
+
+    return obj;
 }
